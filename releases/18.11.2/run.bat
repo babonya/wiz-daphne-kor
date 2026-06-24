04c14da3f235a9bb94eec2a96594a011edc186a8
babonya <babonya@gmail.com>
2026-06-24 16:39:10 +0900

    chore: exclude releases directory and clean index

--- releases/18.11.2/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Wizardry Daphne Antigravity Bot - 18.11.2
-cd /d "%~dp0"
-echo Starting Wizardry Daphne Bot...
-python main.py
-pause
--- releases/WizDapKor_v1.11.7/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Wizardry Daphne Antigravity Bot - 1.11.7
-cd /d "%~dp0"
-echo Starting Wizardry Daphne Bot...
-python main.py
-pause
