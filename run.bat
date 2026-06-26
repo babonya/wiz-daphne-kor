57b696d4acdb2b985aee62ea2ce9df3634d1471d
babonya <babonya@gmail.com>
2026-06-26 13:02:22 +0900

    Release WizDapKor v1.11.10

--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
 @echo off
-title Wizardry Daphne Antigravity Bot - 1.11.8
+title Wizardry Daphne Antigravity Bot - 1.11.10
 cd /d "%~dp0"
+set MACRO_SESSION_START=%date% %time%
 echo Starting Wizardry Daphne Bot...
 python main.py
 pause
